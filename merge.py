4641965ebb543825f2359f069e56b79d7e3bc948
ARUN  KUMAR G <arun.kumar@Kaps-MacBook-Pro.local>
2026-06-16 11:20:44 +0530

    Update merge script

--- merge.py
+++ merge.py
@@ -1,64 +1,43 @@
 import os
-import shutil
-import subprocess
+from huggingface_hub import login
+from unsloth import FastModel
 from dotenv import load_dotenv
-from huggingface_hub import login, HfApi
-from peft import PeftModel
-from transformers import Gemma4ForConditionalGeneration, AutoProcessor
-import torch
-import torch.nn as nn
-
 load_dotenv()
-login(token=os.getenv("HF_TOKEN"))
 
-MERGED_DIR  = ""
-ADAPTER_DIR = ""
-HF_REPO     = ""
-BASE_MODEL  = ""
+HF_TOKEN = os.getenv("HF_TOKEN")
 
-shutil.rmtree(MERGED_DIR, ignore_errors=True)
+BASE_MODEL = "unsloth/gemma-4-e4b-it-unsloth-bnb-4bit"
+ADAPTER_REPO = "ArunK-2003/Gemma4FT_v0"
 
-print("Loading base model...")
-model = Gemma4ForConditionalGeneration.from_pretrained(
-    BASE_MODEL,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-)
-processor = AutoProcessor.from_pretrained(BASE_MODEL)
+MERGED_DIR = "./merged_model"
+HF_MERGED_REPO = "ArunK-2003/Gemma4_Tamil"
 
-print("Unwrapping Gemma4ClippableLinear layers...")
-for name, module in model.named_modules():
-    for child_name, child in list(module.named_children()):
-        if type(child).__name__ == "Gemma4ClippableLinear":
-            setattr(module, child_name, child.linear)
+login(token=HF_TOKEN)
 
-print("Loading adapter...")
-model = PeftModel.from_pretrained(model, ADAPTER_DIR)
-
-print("Merging adapter into base model...")
-model = model.merge_and_unload()
-
-print("Saving merged model...")
-model.save_pretrained(MERGED_DIR, safe_serialization=True)
-processor.save_pretrained(MERGED_DIR)
-
-print("Files after merge:")
-subprocess.run(["ls", "-lh", MERGED_DIR])
-
-print("Creating HF repo if not exists...")
-api = HfApi()
-api.create_repo(
-    repo_id=HF_REPO,
-    repo_type="model",
-    exist_ok=True,
-    token=os.getenv("HF_TOKEN"),
+model, tokenizer = FastModel.from_pretrained(
+    model_name=BASE_MODEL,
+    max_seq_length=8192,
+    load_in_4bit=True,
 )
 
-print(f"Uploading to {HF_REPO}...")
-api.upload_folder(
-    folder_path=MERGED_DIR,
-    repo_id=HF_REPO,
-    repo_type="model",
-    token=os.getenv("HF_TOKEN"),
+model.load_adapter(ADAPTER_REPO)
+os.makedirs(MERGED_DIR, exist_ok=True)
+
+print("Saving merged 16-bit model locally...")
+model.save_pretrained_merged(
+    MERGED_DIR,
+    tokenizer,
+    save_method="merged_16bit"
 )
-print(f"Done! Model pushed to https://huggingface.co/{HF_REPO}")
+print("Local merged model saved.")
+
+
+print("Pushing merged model to HF...")
+model.push_to_hub_merged(
+    HF_MERGED_REPO,
+    tokenizer,
+    save_method="merged_16bit",
+    maximum_memory_usage=0.5,
+    token=HF_TOKEN,
+)
+print(f"Done: https://huggingface.co/{HF_MERGED_REPO}")
